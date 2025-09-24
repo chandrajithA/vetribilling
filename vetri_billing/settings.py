f5803abc7ae20d755b3700a250caa7c53182ed4c
Chandrajith <chandruofficial97@gmail.com>
2025-09-24 16:02:31 +0530

    Initial commit

--- vetri_billing/settings.py
+++ vetri_billing/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env("SECRET_KEY", default="insecure-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[".onrender.com", "localhost", "127.0.0.1"])
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["'vetribilling.onrender.com'", "localhost", "127.0.0.1"])
 
 
 # Application definition
